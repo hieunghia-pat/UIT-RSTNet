c7951130ea20bffa3e77d649df4513bdfee53af9
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-07-27 10:39:34 +0700

    added map for vocab

--- models/rstnet/language_model.py
+++ models/rstnet/language_model.py
@@ -34,7 +34,7 @@ class EncoderLayer(Module):
 
 
 class LanguageModel(Module):
-    def __init__(self, padding_idx=0, bert_hidden_size=768, vocab_size=10201, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, max_len=54, dropout=.1):
+    def __init__(self, vocab, padding_idx=0, bert_hidden_size=768, vocab_size=10201, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, max_len=54, dropout=.1):
         super(LanguageModel, self).__init__()
         self.padding_idx = padding_idx
         self.d_model = d_model
@@ -42,6 +42,10 @@ class LanguageModel(Module):
         self.language_model = RobertaModel.from_pretrained("vinai/phobert-base", return_dict=True)
         self.language_model.config.vocab_size = vocab_size
         self.proj_to_caption_model = nn.Linear(bert_hidden_size, d_model)
+
+        self.vocab = vocab
+        self.pretrained_language_idx_mapping = vocab.pretrained_language_idx_mapping
+        self.pretrained_padding_idx = vocab.pretrained_language_idx_mapping[self.vocab.padding_idx]
 
         self.pos_emb = nn.Embedding.from_pretrained(sinusoid_encoding_table(max_len + 1, d_model, 0), freeze=True)
         self.encoder_layer = EncoderLayer(d_model, d_k, d_v, h, d_ff, dropout)
@@ -56,29 +60,38 @@ class LanguageModel(Module):
         output_hidden_states=False, return_dict=False, encoder_hidden_states=None,
         encoder_attention_mask=None
     ):
+        pretrained_input_ids = []
+        
+        for input_id in input_ids:
+            pretrained_input_id = [self.pretrained_language_idx_mapping[str(ori_id.item())] for ori_id in input_id]
+            pretrained_input_ids.append(pretrained_input_id)
+
+        # List -> tensor.
+        pretrained_input_ids = torch.tensor(pretrained_input_ids).to(input_ids.device)
+
         # input (b_s, seq_len)
-        b_s, seq_len = input_ids.shape[:2]
-        mask_queries = (input_ids != self.padding_idx).unsqueeze(-1).float()  # (b_s, seq_len, 1)
-        mask_self_attention = torch.triu(torch.ones((seq_len, seq_len), dtype=torch.uint8, device=input_ids.device), diagonal=1)
+        b_s, seq_len = pretrained_input_ids.shape[:2]
+        mask_queries = (pretrained_input_ids != self.pretrained_padding_idx).unsqueeze(-1).float()  # (b_s, seq_len, 1)
+        mask_self_attention = torch.triu(torch.ones((seq_len, seq_len), dtype=torch.uint8, device=pretrained_input_ids.device), diagonal=1)
         mask_self_attention = mask_self_attention.unsqueeze(0).unsqueeze(0)  # (1, 1, seq_len, seq_len)
-        mask_self_attention = mask_self_attention + (input_ids == self.padding_idx).unsqueeze(1).unsqueeze(1).byte()
+        mask_self_attention = mask_self_attention + (pretrained_input_ids == self.pretrained_padding_idx).unsqueeze(1).unsqueeze(1).byte()
         mask_self_attention = mask_self_attention.gt(0)  # (b_s, 1, seq_len, seq_len)
         if self._is_stateful:
             self.running_mask_self_attention = torch.cat([self.running_mask_self_attention.type_as(mask_self_attention), mask_self_attention], -1)
             mask_self_attention = self.running_mask_self_attention
-        seq = torch.arange(1, seq_len + 1).view(1, -1).expand(b_s, -1).to(input_ids.device)  # (b_s, seq_len)
+        seq = torch.arange(1, seq_len + 1).view(1, -1).expand(b_s, -1).to(pretrained_input_ids.device)  # (b_s, seq_len)
         seq = seq.masked_fill(mask_queries.squeeze(-1) == 0, 0)
         if self._is_stateful:
             self.running_seq.add_(1)
             seq = self.running_seq
 
         if attention_mask is None:
-            attention_mask = torch.ones_like(input_ids)
+            attention_mask = torch.ones_like(pretrained_input_ids)
         if token_type_ids is None:
-            token_type_ids = torch.zeros_like(input_ids).long()
+            token_type_ids = torch.zeros_like(pretrained_input_ids).long()
 
         bert_output = self.language_model(
-            input_ids=input_ids,
+            input_ids=pretrained_input_ids,
             token_type_ids=token_type_ids,
             attention_mask=attention_mask
         )
